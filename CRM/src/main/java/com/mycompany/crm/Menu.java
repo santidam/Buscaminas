5ecbd1da74de4a20e2689a3876b20cb0ea4dbded
Jordi Hornos <jordi.hrnsllorente@gmail.com>
2024-03-16 12:25:39 +0100

    Implementación de Switch-Case en lugar IF; cambio el código a la clase Validations

--- CRM/src/main/java/com/mycompany/crm/Menu.java
+++ CRM/src/main/java/com/mycompany/crm/Menu.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.crm.controller;
+package com.mycompany.crm;
 
 import com.mycompany.crm.validator.Validations;
 /**
@@ -14,73 +14,38 @@ public class Menu {
     private Validations val = new Validations();
     public void start(String[] args) {
 
+        try {
+            switch(args[0].toLowerCase()){
+                case "altacliente":
+                    val.valAltaCliente(args);
+                    break;
 
-        if (args[1].equalsIgnoreCase("altaCliente")) {
-            if (val.valLength(args.length, 6)) {
-                if(val.valPhone(args[2])) {
-                    if(val.valName(args[3], "nombre")) {
-                        if (val.valName(args[4], "apellido")){
-                            if (val.valEmail(args[5])){
-                                System.out.println("Cliente dado de alta con éxito.");
-                            }
-                        }
-                    }
-                }
+                case "altaempleado":
+                    val.valAltaEmpleado(args);
+                    break;
+
+                case "bajaempleado":
+                    val.valBajaEmpleado(args);
+                    break;
+
+                case "cliente":
+                    val.valClienteInfo(args);
+                    break;
+
+                case "empleado":
+                    val.valEmpleadoInfo(args);
+                    break;
+
+                case "listclientes":
+                    val.valClientesList(args);
+                    break;
+
+                case "listempleados":
+                   val.valEmpleadosList(args);
+                    break;
             }
+        }catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("ERROR. No has puesto ningun argumento");
         }
-
-
-        if (args[1].equalsIgnoreCase("altaEmpleado")) {
-            if (val.valLength(args.length, 7)) {
-                if (val.valDni(args[2])) {
-                    if (val.valName(args[3], "nombre")) {
-                        if (val.valName(args[4], "apellido")) {
-                            if (val.area) {
-                                if (val.posicion) {
-                                    System.out.println("Empleado dado de alta con éxito.");
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        if (args[1].equalsIgnoreCase("bajaEmpleado")) {
-            if (val.valLength(args.length, 3)) {
-                if (val.valDni(args[2])) {
-                    System.out.println("Empleado dado de baja con éxito.");
-                }
-            }
-        }
-
-        if (args[1].equalsIgnoreCase("cliente")) {
-            if (val.valLength(args.length, 3) {
-                if (val.valPhone(args[2])) {
-                    System.out.println("Esta es toda la información del cliente con teléfono:" + args[2]);
-                }
-            }
-        }
-
-        if (args[1].equalsIgnoreCase("empleado")) {
-            if (val.valLength(args.length, 3)) {
-                if (val.valDni(args[2])) {
-                    System.out.println("Esta es toda la información del empleado con DNI:" + args[2]);
-                }
-            }
-        }
-
-        if (args[1].equalsIgnoreCase("listClientes")) {
-            if (val.valLength(args.length, 2)) {
-                System.out.println("Esta es la lista de todos los clientes:");
-            }
-        }
-
-        if (args[1].equalsIgnoreCase("listEmpleados")) {
-            if (val.valLength(args.length, 2)) {
-                System.out.println("Esta es la lista de todos los empleados:");
-            }
-        }
-
     }
 }
